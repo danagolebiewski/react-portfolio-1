8435a4597aa3e7ca1ef4c480a1b8392ceb6c1f4e
EsotericZ <cjsand03@gmail.com>
2022-03-21 16:01:06 -0600

    backgrounds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import {
-  Navbar,
-} from './components/';
+import React from 'react';
+import Particles from "react-tsparticles";
+import particlesOptions from "./particles.json";
 
 function App() {
-  return (
-    <>
-      <Navbar/>
-    </>
-  );
+    return (
+        <div className="App">
+            <Particles options={particlesOptions}/>
+            {/* <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <a
+                    className="App-link"
+                    href="https://particles.js.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    See Particles samples
+                </a>
+            </header> */}
+        </div>
+    );
 }
 
 export default App;
