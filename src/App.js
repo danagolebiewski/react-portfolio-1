bdd9c920fb714d580ab85e802b93f99b854a7b88
EsotericZ <cjsand03@gmail.com>
2022-03-22 07:06:03 -0600

    cleaned up all the files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import React from 'react';
 import Particles from "react-tsparticles";
-import particlesOptions from "./particles.json";
+import particlesOptions from "./background/particles.json";
 
-function App() {
+const App = () => {
     return (
         <div className="App">
-            <Particles options={particlesOptions}/>
-            {/* <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header> */}
+            <Particles options={particlesOptions} />
         </div>
     );
 }
