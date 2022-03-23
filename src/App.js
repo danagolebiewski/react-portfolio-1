e51cf489bd5f36aea19dc6be0e2bb79aae5fabcc
EsotericZ <cjsand03@gmail.com>
2022-03-23 14:25:43 -0600

    removed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Particles from "react-tsparticles";
 import particlesOptions from "./background/particles.json";
-import PortNav from "./components/PortNav";
 
 const App = () => {
     return (
         <div>
-            <PortNav />
             <Particles options={particlesOptions} />
         </div>
     );
