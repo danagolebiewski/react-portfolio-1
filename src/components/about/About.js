727211a27c8f92346ddab9fc1aa3cdd78ab7ba09
EsotericZ <cjsand03@gmail.com>
2022-03-28 13:05:44 -0600

    projects

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,50 @@
 import './about.css';
+import Resume from '../Resume';
+// import CJ from '../../assets/xx'
 
 const About = () => {
     return (
         <section id='about'>
-            About
+            <h2>About Me</h2>
+
+            {/* <div className='container about__container'>
+                <div className='about__me'>
+                    <div className='about__me-image'>
+                        <img src={CJ} alt="image" />
+                    </div>
+                </div>
+            </div> */}
+
+            <div className='container about__content'>
+                <p>
+                Currently working as an Engineer for a manufacturing company producing and maintainig our internal company website.
+I have been working within Python for the last 3 years and have become proficient in SQL, Flask, Pandas, git and much more.
+
+I enrolled at Denver University boot camp in October of 2021 to build upon my coding knowledge and make the switch into Full Stack Web Development.
+
+I look forward to helping you build your next website!
+                </p>
+                <div className='about__cards'>
+                    <article className='about__card'>
+                        <h5>Languages</h5>
+                        <small>JavaScript / JQuery</small>
+                        <small>NodeJS</small>
+                        <small>ReactJS</small>
+                        <small>Python 3.x</small>
+                        <small>Flask</small>
+                        <small>Pandas</small>
+                    </article>
+                    <article className='about__card'>
+                        <h5>Databases</h5>
+                        <small>MySQL</small>
+                        <small>SQLite</small>
+                        <small>PostgreSQL</small>
+                        <small>MongoDB</small>
+                    </article>
+                </div>
+                <a href='#contact' className='btn btn-primary'>Contact Me</a>
+                <Resume />
+            </div>
         </section>
     )
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,78 @@
 import './projects.css';
+import prImg1 from '../../assets/prImg1.png';
+import prImg2 from '../../assets/prImg2.png';
+import prImg3 from '../../assets/prImg3.png';
+import prImg4 from '../../assets/prImg4.png';
+import prImg5 from '../../assets/prImg5.png';
+import prImg6 from '../../assets/prImg6.png';
+
+const cjProjects = [
+    {
+        id: 1,
+        image: prImg1,
+        titile: 'FamYOUly',
+        github: 'https://github.com/',
+        demo: 'https://github.com',
+    },
+    {
+        id: 2,
+        image: prImg2,
+        titile: 'FamYOUly',
+        github: 'https://github.com/',
+        demo: 'https://github.com',
+    },
+    {
+        id: 3,
+        image: prImg3,
+        titile: 'FamYOUly',
+        github: 'https://github.com/',
+        demo: 'https://github.com',
+    },
+    {
+        id: 4,
+        image: prImg4,
+        titile: 'FamYOUly',
+        github: 'https://github.com/',
+        demo: 'https://github.com',
+    },
+    {
+        id: 5,
+        image: prImg5,
+        titile: 'FamYOUly',
+        github: 'https://github.com/',
+        demo: 'https://github.com',
+    },
+    {
+        id: 6,
+        image: prImg6,
+        titile: 'FamYOUly',
+        github: 'https://github.com/',
+        demo: 'https://github.com',
+    },
+]
 
 const Projects = () => {
     return (
         <section id='projects'>
-            Projects
+            <h2>Projects</h2>
+
+            <div className='container project__container'>
+                { cjProjects.map(({id, image, title, github, demo}) => {
+                    return (
+                        <article key={ id } className='project__item'>
+                            <h3>{ title }</h3>
+                            <div className='project__item-image'>
+                                <img src={ image } alt={ title } />
+                            </div>
+                            <div className='project__item-cta'>
+                                <a href={ github } className='btn' target='_blank'>Github</a>
+                                <a href={ demo } className='btn btn-primary' target='_blank'>Live Demo</a>
+                            </div>
+                        </article>
+                    )
+                })}
+
+            </div>
         </section>
     )
 }
